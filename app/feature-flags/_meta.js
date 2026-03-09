6e96222f63d1717cb377b36b454e5ff334a5456e
Alex Griffiths <axgriffiths67@gmail.com>
2026-03-08 21:12:05 -0500

    Feature flag logging

--- app/feature-flags/_meta.js
+++ app/feature-flags/_meta.js
@@ -2,5 +2,6 @@ export default {
   index: 'Overview',
   segments: 'Segments',
   sdk: 'SDK',
+  'health-reporter': 'Health Reporter',
   openfeature: 'OpenFeature Provider',
 }
--- next.config.mjs
+++ next.config.mjs
@@ -3,5 +3,5 @@ import nextra from 'nextra'
 const withNextra = nextra({})
 
 export default withNextra({
-  output: 'export',
+  //output: 'export',
 })
